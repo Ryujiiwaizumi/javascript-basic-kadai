9bdff5d168e785154c816a5e1c391bdb8af89bc7
Ryujiiwaizumi <142085167+Ryujiiwaizumi@users.noreply.github.com>
2023-08-29 21:36:39 +0900

    定数を使用してのbmiの計算を追加しました。

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -4,8 +4,11 @@ let heigh = 1.7;
 //変数(体重)の宣言・代入
 let bodyWeight = 68;
 
+//bmiの計算式
+const bmi= bodyWeight/(heigh*heigh);
+
 //bmiの計算出力
-console.log(bodyWeight/(heigh*heigh));
+console.log(bmi);
 
 
 
